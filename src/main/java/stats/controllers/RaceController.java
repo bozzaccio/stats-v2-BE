cb44fff5654a31f344dfd60671042c046c8603cb
bozzaccio <74464364+bozzaccio@users.noreply.github.com>
2021-01-18 10:48:53 +0100

    Update RaceController.java

--- src/main/java/stats/controllers/RaceController.java
+++ src/main/java/stats/controllers/RaceController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin("*")
 public class RaceController {
 
     @Autowired
